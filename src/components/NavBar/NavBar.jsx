c65402e739fe1e4f09318bb765f1f4c43c89816d
Aurel Ferrand <ferrand.aurel@gmail.com>
2022-03-21 14:32:36 +0100

    Navbar mobile css + icon

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,27 +1,50 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { BsShop } from "react-icons/bs";
+import { AiOutlineHome, AiOutlinePhone } from "react-icons/ai";
+import { RiAccountPinCircleLine } from "react-icons/ri";
+import { FcStatistics } from "react-icons/fc";
+import { BsInfoCircle } from "react-icons/bs";
+
 import "./NavBar.css";
 
 const NavBar = () => {
   return (
-    <nav>
-      <a className="nav-home" href="#">
-        Petit.Mousquetaire
-      </a>
-      <ul>
-        <li>
-          <a href="#" class="active">
-            Mon Compte
-          </a>
-        </li>
-        <li>
-          <a href="#">Stats</a>
-        </li>
-        <li>
-          <a href="#">Information</a>
-        </li>
-        <li>
-          <a href="#">contact</a>
-        </li>
+    <nav className="navbar">
+      <Link to="/">
+        <span className="nav-home">Petit.Mousquetaire</span>
+      </Link>
+      <ul className="menu">
+        <Link to="/">
+          <li>
+            <AiOutlineHome className="nav-icon" />
+            <span className="nav-text mobile">Accueil</span>
+          </li>
+        </Link>
+        <Link to="/account">
+          <li>
+            <RiAccountPinCircleLine className="nav-icon" />
+            <span className="nav-text active">Mon Compte</span>
+          </li>
+        </Link>
+        <Link to="/stats">
+          <li>
+            <FcStatistics className="nav-icon" />
+            <span className="nav-text">Stats</span>
+          </li>
+        </Link>
+        <Link to="/information">
+          <li>
+            <BsInfoCircle className="nav-icon" />
+            <span className="nav-text">information</span>
+          </li>
+        </Link>
+        <Link to="/contact">
+          <li>
+            <AiOutlinePhone className="nav-icon" />
+            <span className="nav-text">contact</span>
+          </li>
+        </Link>
       </ul>
     </nav>
   );
